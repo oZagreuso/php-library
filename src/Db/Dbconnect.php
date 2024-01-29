836d34be8dad22db1322bb69614084b4e1ff43ae
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2024-01-29 16:51:34 +0100

    290124PM1651

--- src/Db/Dbconnect.php
+++ src/Db/Dbconnect.php
@@ -1,6 +1,7 @@
 <?php 
 
-namespace nomDutilisateurGithub\Db;
+namespace Odalferro\Db;
+use Odalferro\Db\config;
 
 use PDO; 
 
@@ -9,6 +10,7 @@ use PDO;
  */
 class DbConnect
 {
+    private static array $config = [];
     /**
      * Constructeur privé (la classe n'est pas instanciable)
      */
@@ -17,6 +19,8 @@ class DbConnect
     /** @var PDO $instance l'instance PDO unique */
     private static ?PDO $instance = null;
 
+   
+
     /**
      * Récupère l'instance unique de PDO
      * La crée si elle n'existe pas encore
@@ -24,13 +28,22 @@ class DbConnect
      */
     public static function getInstance(): PDO
     {
-        if(self::$instance === null) {
-            self::$instance = new PDO('mysql:host=localhost;port=3306;dbname=db_cars;charset=utf8', 'root', '', [
+        if(self::$instance === null) 
+        {
+            self::$instance = new PDO('mysql:host=' . self::$config['db_host'] . ';port=' . self::$config['db_port'] . ';dbname=' . self::$config['db_name'] . ';charset=utf8', self::$config['db_user'], self::$config['db_password'], [
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]);
         }
         return self::$instance;
+    }
+
+    
+
+    public static function setConfiguration(string $chemin_vers_le_fichier): void
+    {        
+        $config = require $chemin_vers_le_fichier;
+        self::$config = $config;
     }
 }
---
+++ src/Db/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Odalferro\Db;
+
+return [
+    'db_host' => 'localhost',
+    'db_port' => '3306',
+    'db_name' => 'db_cars',
+    'db_user' => 'root',
+    'db_password' => '',
+];
